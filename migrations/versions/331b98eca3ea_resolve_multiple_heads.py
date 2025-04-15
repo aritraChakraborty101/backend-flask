87b7f86e0384b7cd1cfb7802344c2dca53de9288
Aritra Chakraborty <aritra.chakraborty@g.bracu.ac.bd>
2025-04-15 08:48:51 +0600

    git conflict and database conflict resolved

---
+++ migrations/versions/331b98eca3ea_resolve_multiple_heads.py
@@ -0,0 +1,24 @@
+"""resolve multiple heads
+
+Revision ID: 331b98eca3ea
+Revises: 
+Create Date: 2025-04-15 08:33:53.696054
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '331b98eca3ea'
+down_revision = ('62ad45e3bcba', 'a76ae7ef1464')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
